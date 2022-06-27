163ad8c62069bbd4c219d18459b488aac3a72f56
AndyAvta <asavtamonov@edu.hse.ru>
2022-06-27 15:16:55 +0300

    pas rez exist

---
+++ test.h
@@ -0,0 +1,76 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <QTest>
+#include <QObject>
+#include "ai.h"
+#include "game.h"
+#include <QFrame>
+#include <QtGui>
+#include <record.h>
+#include "mw.h"
+#include <qtextcodec.h>
+
+class Test: public QObject
+ {
+  Q_OBJECT
+
+  private:
+   bool Condition()
+    {
+     return true;
+    }
+  private slots:
+   void initTestCase()
+    {
+     qDebug("Запуск тестов...");
+    }
+   void FirstTest()
+    {
+     QVERIFY(Condition());
+     QCOMPARE(10, 10);
+    }
+   void cbp_1_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+     QCOMPARE(9, 9);
+    }
+   void cbp_2_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+     QCOMPARE(6, 6);
+    }
+   void cbai_11_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+   void cbai_12_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+   void cbai_13_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+   void cbai_21_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+   void cbai_22_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+   void cbai_23_Test()
+    {
+     QVERIFY(Condition());
+     QVERIFY(1==1);
+    }
+ };
+#endif // TEST_H
